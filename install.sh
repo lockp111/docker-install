ad1c338425a54481726f2f16dddeaa2f9a263d9f
Harman.P <lockp111@protonmail.com>
2025-02-23 19:35:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/doc
 chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
+mkdir -p /etc/apt/sources.list.d
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
